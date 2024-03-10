650960537b7d9321519e0e2c370ef699d4109bf3
ArnavMeh <mehraarn000@gmail.com>
2024-03-10 03:30:10 -0400

    timer for perf test

--- vsynth-unity/Assets/Scripts/Hello.cs
+++ vsynth-unity/Assets/Scripts/Hello.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
 using static Assets.Scripts.Test.Test;
+using Assets.Scripts.Test;
 
 public class Hello : MonoBehaviour {
     void Start() {
+        /*
         var synth = new Synth(
             new() {
                 new Vector3(1, 0, 3),
@@ -11,8 +13,8 @@ public class Hello : MonoBehaviour {
             },
             new Vector3(0.5f, 0, 1.5f)
         );
-
-        /*
+        */
+        
         var synth = new Synth(
             new() {
                 UnityEngine.Random.insideUnitSphere,
@@ -20,8 +22,7 @@ public class Hello : MonoBehaviour {
             },
             null
         );
-        */
 
-        TestFind(synth, 8);
+        TestGen(synth, 7);
     }
 }
--- vsynth-unity/Assets/Scripts/VSynth/Test.cs
+++ vsynth-unity/Assets/Scripts/VSynth/Test.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 using static Assets.Scripts.Ops.Types;
 
 namespace Assets.Scripts.Test {
+    public class Timer {
+        public static System.Diagnostics.Stopwatch sw;
+
+        public static void Start() {
+            sw = System.Diagnostics.Stopwatch.StartNew();
+        }
+
+        public static long End() {
+            return sw.ElapsedMilliseconds;
+        }
+    }
+
 	public class Test {
 		public static void TestFind(Synth synth, int complexity) {
+            Timer.Start();
             var (res, ls) = synth.FindAST(complexity);
+            float t = Timer.End() / 1000.0f;
 
             ValueTuple<float, AST> best = (float.PositiveInfinity, new(null));
             ls.ForEach(a => {
@@ -24,11 +38,17 @@ namespace Assets.Scripts.Test {
                 + "\nComplexity: " + best.Item2.complexity
                 + "\nErr: " + best.Item1
                 + "\n"
+                + "\nElapsed seconds: " + t
+                + "\nPerformance: " + (ls.Count / t) + " ASTs/s"
+                + "\n"
             );
         }
 
         public static void TestGen(Synth synth, int complexity) {
+            Timer.Start();
             var res = synth.GenASTs(complexity);
+            float t = Timer.End() / 1000.0f;
+
             int vec_ret_cnt = res.FindAll(a => a.RetType() == OpType.Vec).Count;
             int flt_ret_cnt = res.Count - vec_ret_cnt;
 
@@ -43,14 +63,20 @@ namespace Assets.Scripts.Test {
                 + "\nVecFltCnt: " + synth.VecFltCnt
                 + "\nFltFltCnt: " + synth.FltFltCnt
                 + "\n"
-                + "\nExample ASTs:\n"
+                + "\nElapsed seconds: " + t
+                + "\nPerformance: " + (res.Count / t) + " ASTs/s"
+                + "\n"
             );
-            for (int i = 0; i < res.Count; i++) {
+            
+            //str += "\nExample ASTs:\n";
+            //for (int i = 0; i < res.Count; i++) {
                 //AST a = res[(int)Math.Floor(UnityEngine.Random.value * res.Count)];
-                AST a = res[i];
-                str += a.complexity + ": " + synth.StringifyAST(a) + "\n";
-            }
+            //    AST a = res[i];
+            //    str += a.complexity + ": " + synth.StringifyAST(a) + "\n";
+            //}
+
             Debug.Log(str);
+
         }
 	}
 }
